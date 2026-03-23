c531239c3d6a5111ec4ee89e2e35645d92a33864
Anurag Borkar <anuragborkar2005@gmail.com>
2026-03-23 04:30:29 +0000

    add landing hero section

--- context/index.tsx
+++ context/index.tsx
@@ -4,7 +4,7 @@ import { wagmiAdapter, projectId } from "@/config"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { createAppKit } from "@reown/appkit/react"
 import { sepolia } from "@reown/appkit/networks"
-import React, { type ReactNode } from "react"
+import { type ReactNode } from "react"
 import { cookieToInitialState, WagmiProvider, type Config } from "wagmi"
 
 // Set up queryClient
